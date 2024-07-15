878a309129aa9d1753f807b80ed89f7a9979bb82
Connie Leung <cleung334@gmail.com>
2024-07-15 09:24:43 +0800

    feat: add configurations of Gemini AI

--- src/configs/configuration.ts
+++ src/configs/configuration.ts
@@ -4,4 +4,8 @@ export default () => ({
     apiKey: process.env.GROQ_API_KEY || '',
     model: process.env.GROQ_MODEL || 'gemma2-9b-it',
   },
+  gemini: {
+    apiKey: process.env.GEMINI_API_KEY || '',
+    model: process.env.GEMINI_TEXT_EMBEDDING_MODEL || 'text-embedding-004',
+  },
 });
