d736108fc0801c735e282877bf44115a6ab52f10
Connie Leung <cleung334@gmail.com>
2024-07-21 15:30:41 +0800

    feat: qdrant store

--- src/vector-store/application/vector-store.service.ts
+++ src/vector-store/application/vector-store.service.ts
@@ -1,54 +1,47 @@
+import { DocumentInterface } from '@langchain/core/documents';
 import { Embeddings } from '@langchain/core/embeddings';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import path from 'path';
 import { appConfig } from '~configs/root-path.config';
-import { ANGULAR_EVOLUTION_BOOK, TEXT_EMBEDDING_MODEL, VECTOR_STORE_TYPE } from './constants/rag.constant';
+import { ANGULAR_EVOLUTION_BOOK, TEXT_EMBEDDING_MODEL, VECTOR_DATABASE } from './constants/rag.constant';
+import { VectorStoreContract } from './interfaces/vector-store-contract.interface';
 import { loadPdf } from './loaders/pdf-loader';
-import { VectorStore } from '@langchain/core/vectorstores';
-import { createVectorStore } from './vector-stores/create-store';
-import { VectorStoresType } from './types/vector-stores.type';
 
 @Injectable()
 export class VectorStoreService {
-  private vectorStore: VectorStore;
+  private readonly logger = new Logger(VectorStoreService.name);
 
   constructor(
-    @Inject(TEXT_EMBEDDING_MODEL) private embeddings: Embeddings,
-    @Inject(VECTOR_STORE_TYPE) type: VectorStoresType,
+    @Inject(TEXT_EMBEDDING_MODEL) embeddings: Embeddings,
+    @Inject(VECTOR_DATABASE) private vectorStoreService: VectorStoreContract,
   ) {
-    this.createVectorStore(type, this.embeddings);
+    this.createVectorStore(embeddings, this.vectorStoreService);
   }
 
-  private async createVectorStore(type: VectorStoresType, embeddings: Embeddings) {
+  private async createVectorStore(embeddings: Embeddings, vectoreStoreService: VectorStoreContract) {
     const docs = await this.loadDocuments();
-    this.vectorStore = await createVectorStore({ docs, type, embeddings });
+    await vectoreStoreService.init({ docs, embeddings });
   }
 
   private async loadDocuments() {
     const bookFullPath = path.join(appConfig.rootPath, ANGULAR_EVOLUTION_BOOK);
-    console.log(bookFullPath);
     const docs = await loadPdf(bookFullPath);
-    console.log('number of docs', docs.length);
-    if (docs.length > 0) {
-      const firstPage = docs[0];
-      const lastPage = docs[docs.length - 1];
-      console.log("first page's metadata", firstPage.metadata);
-      console.log('first page', firstPage.pageContent);
-      console.log("last page's metadata", lastPage.metadata);
-      console.log('last page', lastPage.pageContent);
-    }
+    this.logger.log(`number of docs -> ${docs.length}`);
+    // if (docs.length > 0) {
+    //   const firstPage = docs[0];
+    //   console.log("first page's metadata", firstPage.metadata);
+    //   console.log('first page', firstPage.pageContent);
+    // }
     return docs;
   }
 
-  // async createStore() {
-  //   await this.loadDocuments();
-  // }
-
-  async testEmbedding(): Promise<number[]> {
-    return this.embeddings.embedQuery('Register embedding model in NestJS is OK.');
+  similaritySearchWithScore(query: string, numResults = 1): Promise<{ doc: DocumentInterface; score: number }[]> {
+    this.logger.log(`similaritySearchWithScore query -> ${query}, numResults -> ${numResults}`);
+    return this.vectorStoreService.similaritySearchWithScore({ query, numResults });
   }
 
-  async testVectorStore() {
-    return this.vectorStore.similaritySearchWithScore('New control flow', 3);
+  similaritySearch(query: string, numResults = 1): Promise<DocumentInterface[]> {
+    this.logger.log(`similaritySearch query -> ${query}, numResults -> ${numResults}`);
+    return this.vectorStoreService.similaritySearch({ query, numResults });
   }
 }
