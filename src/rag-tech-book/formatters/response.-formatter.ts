ef60cd3068310a27d7aacd69405b36249c14415a
Connie Leung <cleung334@gmail.com>
2024-07-31 01:29:54 +0800

    chore: delete tailwindcss dependency

--- src/rag-tech-book/formatters/response.-formatter.ts
+++ src/rag-tech-book/formatters/response.-formatter.ts
@@ -4,9 +4,9 @@ export const toDivRow = (contents: ConversationContent[]): string => {
   return contents
     .map(
       ({ role, content }) => `
-      <div class="response text-[1rem] flex text-[#464646]">
+      <div class="text-[1rem] flex text-[#464646]">
         <span class="w-1/5 p-1 border border-solid border-[#464646]">${role}</span>
-        <span class="w-4/5 p-1 border border-solid border-[#464646]">${content}</span>
+        <span class="w-4/5 p-1 border border-solid border-[#464646] prose">${content}</span>
       </div>`,
     )
     .join('');
